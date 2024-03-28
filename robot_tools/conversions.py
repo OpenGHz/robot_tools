9dcf1461fcbcf8033928be3408729fcce33c520f
OpenGHz <1352674740@qq.com>
2024-03-28 16:14:54 +0800

    update

--- robot_tools/conversions.py
+++ robot_tools/conversions.py
@@ -33,7 +33,7 @@
 # Author: Ioan Sucan
 
 from io import BytesIO as StringIO
-from geometry_msgs.msg import Pose, PoseStamped, Transform
+from geometry_msgs.msg import Pose, PoseStamped, Transform, Twist
 import rospy
 from . import transformations
 
@@ -118,3 +118,16 @@ def list_to_transform(trf_list):
     trf_msg.rotation.z = trf_list[5]
     trf_msg.rotation.w = trf_list[6]
     return trf_msg
+
+def to_Twist(linear_all, angular=(0, 0, 0)):
+    """将线速度和角速度转换为Twist消息"""
+    msg = Twist()
+    if len(linear_all) == 6:
+        angular = linear_all[3:]
+    msg.linear.x = linear_all[0]
+    msg.linear.y = linear_all[1]
+    msg.linear.z = linear_all[2]
+    msg.angular.x = angular[0]
+    msg.angular.y = angular[1]
+    msg.angular.z = angular[2]
+    return msg
